368e4f2ecfbe362d617c0b0a0843901d888b4cef
Paige Hetherington <herbmama@gmail.com>
2016-03-29 10:43:30 -0400

    create grid

--- src/maze_clojure/core.clj
+++ src/maze_clojure/core.clj
@@ -1,5 +1,33 @@
 (ns maze-clojure.core
   (:gen-class))
 
+(def size 10) ; grid size 10x10
+
+(defn create-rooms []
+  (vec
+    (for [row (range size)]  ; a vec of vecs, for loop inside of for loop
+      (vec
+        (for [col (range size)]
+          {:row row, :col col, :visited? false, :bottom? true, :right? true})))))
+  
+  
+
 (defn -main []
-  (println "Hello, World!"))
+  (let [rooms (create-rooms)]
+    ;print top walls
+    (doseq [_ rooms]
+      (print " _")) ; space first for vertical wall
+    (println);makes new line after first wall
+    ;print grid
+    (doseq [row rooms] ;doseq (not lazy) loops over rooms and creates temp variable row
+      (print "|")
+      (doseq [room row] ; loops over row to create room
+        (if (:bottom? room)
+          (print "_") ;if true print _
+          (print " ")) ;if false print " "
+        (if (:right? room)
+          (print "|")
+          (print " ")))
+      (println))))
+        
+      
