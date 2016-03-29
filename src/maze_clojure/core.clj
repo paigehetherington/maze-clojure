89a7d82fd368deeb3f719b2678330e48bdb8d5a8
Paige Hetherington <herbmama@gmail.com>
2016-03-29 12:31:29 -0400

    add maze functions

--- src/maze_clojure/core.clj
+++ src/maze_clojure/core.clj
@@ -10,10 +10,54 @@
         (for [col (range size)]
           {:row row, :col col, :visited? false, :bottom? true, :right? true})))))
   
-  
+
+(defn possible-neighbors [rooms row col] ; contains only valid neighbors
+  (vec
+    (remove 
+      (fn [room] ;anon fxn
+        (or(nil? room) (:visited? room)))
+      [(get-in rooms [(dec row) col]) ; one below
+       (get-in rooms [(inc row) col]) ;one row above
+       (get-in rooms [row (dec col)]) ;to left column
+       (get-in rooms [row (inc col)])]))) ; to right column
+
+(defn random-neighbor [rooms row col]
+  (let [neighbors (possible-neighbors rooms row col)]
+    (if (pos? (count neighbors))
+      (rand-nth neighbors)
+      nil)))
+
+(defn tear-down-wall [rooms old-row old-col new-row new-col]
+  (cond
+    ;going up
+    (< new-row old-row) ;new row less than old row
+    (assoc-in rooms [new-row new-col :bottom?] false)
+    ; going down
+    (> new-row old-row)
+    (assoc-in rooms [old-row old-col :bottom?] false)
+    ;going left:
+    (< new-col old-col)
+    (assoc-in rooms [new-row new-col :right?] false)
+    ;going right:
+    (> new-col old-col)
+    (assoc-in rooms [old-row old-col :right?] false)))
+
+(defn create-maze [rooms row col]
+  (let [rooms (assoc-in rooms [row col :visited?] true)
+        next-room (random-neighbor rooms row col)]
+    (if next-room
+        ;call create-maze recursively
+        (loop [old-rooms (tear-down-wall rooms row col (:row next-room) (:col next-room))] ;creates local variable
+          (let [new-rooms (create-maze old-rooms (:row next-room) (:col next-room))]
+            (if (= old-rooms new-rooms)
+              old-rooms
+              (recur new-rooms))))
+      rooms)));else if doesn't find room, stops
+        
 
 (defn -main []
-  (let [rooms (create-rooms)]
+  (let [rooms (create-rooms)
+        rooms (create-maze rooms 0 0)]
     ;print top walls
     (doseq [_ rooms]
       (print " _")) ; space first for vertical wall
